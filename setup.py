d9ca439b056fa7a5be7f53bd1f9d5b443b55f771
bartbruininks <bartbruininks@gmail.com>
2024-07-03 17:20:50 +0200

    Explicitly define packages in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup, Extension, find_packages
+from setuptools import setup, Extension
 from Cython.Build import cythonize
 
 setup(
     name='mdvcontainment',
     version='0.1',
-    packages=find_packages(include=['mdvcontainment']),
+    packages=['mdvcontainment'],
     ext_modules=cythonize(["mdvcontainment/find_label_contacts.pyx", 'mdvcontainment/find_bridges.pyx']),
     install_requires=[
         "numpy",
