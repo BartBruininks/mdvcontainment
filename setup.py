bef39e9bc6c23a78d8b8973c7badf3b65dc2defb
BartBruininks <BartBruininks@gmail.com>
2025-11-19 16:54:08 +0100

    Adds metadata to prepare for pypi deployment

--- setup.py
+++ setup.py
@@ -6,6 +6,12 @@ setup(
     name='mdvcontainment',
     version='0.1.1',
     packages=['mdvcontainment'],
+    author='BMH Bruininks',
+    description = "Containment analysis for (periodic) point clouds.",
+    long_description = 'file: README.md',
+    long_description_content_type = 'text/markdown',
+    url = "https://github.com/BartBruininks/mdvcontainment",
+    python_requires = ">=3.12",
     ext_modules=cythonize(["mdvcontainment/find_label_contacts.pyx", 'mdvcontainment/find_bridges.pyx']),
     include_dirs=[numpy.get_include()],
     install_requires=[
