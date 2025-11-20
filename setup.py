972459a9c7d63a1c99de3c2784b3aa051c762abc
BartBruininks <BartBruininks@gmail.com>
2025-11-20 15:27:25 +0100

    Bump version to v0.1.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='mdvcontainment',
-    version='0.1.1',
+    version='0.1.2',
     packages=['mdvcontainment'],
     author='BMH Bruininks',
     description = "Containment analysis for (periodic) point clouds.",
