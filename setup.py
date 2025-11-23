0cf86ac245fdf4944c5027a7a0c14730a4172403
BartBruininks <BartBruininks@gmail.com>
2025-11-23 20:52:12 +0100

    Fix link to readme in metadata

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     packages=['mdvcontainment'],
     author='BMH Bruininks',
     description = "Containment analysis for (periodic) point clouds.",
-    long_description = 'file: README.md',
+    long_description = 'README.md',
     long_description_content_type = 'text/markdown',
     url = "https://github.com/BartBruininks/mdvcontainment",
     python_requires = ">=3.12",
